913aeaf35a7398ae0d41cd657ef8e1bc76e2af0c
santu <santuchotomas@gmail.com>
2023-12-11 20:46:09 -0300

    pre vercel

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const expressLayouts = require("express-ejs-layouts");
+const methodOverride = require("method-override");
 
 app.use(express.static("public"));
 
@@ -9,6 +10,9 @@ app.set("views", "./src/views");
 
 app.use(expressLayouts);
 app.set("layout", "layouts/layout");
+
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride("_method"));
 
 app.use(require("./src/routes/tareasRouter"));
 
--- src/routes/tareasRouter.js
+++ src/routes/tareasRouter.js
@@ -4,5 +4,8 @@ const router = express.Router();
 const controller = require("../controller/tareasController");
 
 router.get("/", controller.index);
+router.post("/", controller.store);
+router.put("/:id", controller.update);
+router.delete("/:id", controller.destroy);
 
 module.exports = router;
